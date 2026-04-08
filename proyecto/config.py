2451e4c8e39d5c917569b4c3dcaf06ba587bccd4
Jonathan <jonathanreyes_24@hotmail.com>
2026-04-08 15:06:08 -0600

    Responsivo

--- proyecto/config.py
+++ proyecto/config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = os.getenv("SECRET_KEY")   # "b4f1c9e2a7d84c1f93a2e7b6d1c4f8a9e3b7c2d1f4a8b6c3d9e1f2a7c4b8d3e"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")   # "postgresql+psycopg2://postgres:12345@localhost:5433/posts"
     CKEDITOR_PKG_TYPE="full"
