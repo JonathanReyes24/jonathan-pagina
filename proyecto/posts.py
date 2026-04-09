b9cb0dffbc06e11438c4b5c7c7bd86cd550c2516
Jonathan <jonathanreyes_24@hotmail.com>
2026-04-09 09:17:22 -0600

    Actualizar Fecha Posts

--- proyecto/posts.py
+++ proyecto/posts.py
@@ -5,13 +5,14 @@ from . import db
 
 bpPosts =  Blueprint("posts",__name__)
 
+def reemplazar(texto):
+    return texto.replace("January","Enero").replace("enero","Enero").replace("February","Febrero").replace("febrero","Febrero").replace("March","Marzo").replace("marzo","Marzo").replace("April","Abril").replace("abril","Abril").replace("May","Mayo").replace("mayo","Mayo").replace("June","Junio").replace("junio","Junio").replace("July","Julio").replace("julio","Julio").replace("August","Agosto").replace("agosto","Agosto").replace("September","Septiembre").replace("septiembre","Septiembre").replace("October","Octubre").replace("octubre","Octubre").replace("November","Noviembre").replace("noviembre","Noviembre").replace("December","Diciembre").replace("diciembre","Diciembre")
+
 @bpPosts.route("/<url>/")
 def post(url):
     post = Post.query.filter_by(url=url).first()
-    return render_template("post.html",post=post)
+    return render_template("post.html",post=post,reemplazar=reemplazar)
 
-def reemplazar(texto):
-    return texto.replace("January","Enero").replace("enero","Enero").replace("February","Febrero").replace("febrero","Febrero").replace("March","Marzo").replace("marzo","Marzo").replace("April","Abril").replace("abril","Abril").replace("May","Mayo").replace("mayo","Mayo").replace("June","Junio").replace("junio","Junio").replace("July","Julio").replace("julio","Julio").replace("August","Agosto").replace("agosto","Agosto").replace("September","Septiembre").replace("septiembre","Septiembre").replace("October","Octubre").replace("octubre","Octubre").replace("November","Noviembre").replace("noviembre","Noviembre").replace("December","Diciembre").replace("diciembre","Diciembre")
 @bpPosts.route("/posts/",methods=["GET","POST"])
 def posts():
     posts = Post.query.all()
